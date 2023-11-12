3f127df8cb3ccdb18793680b85a40a57ea5d6ce0
agueye84 <adama.gueye-ext@mcn.gouv.qc.ca>
2023-11-11 23:38:18 -0500

    remove react-native-fs

--- src/screens/AddProduct.tsx
+++ src/screens/AddProduct.tsx
@@ -23,7 +23,6 @@ import Button from '../components/Button';
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 import storage from '@react-native-firebase/storage';
-import fs from 'react-native-fs';
 import {
   descriptionValidator,
   deviseValidator,
